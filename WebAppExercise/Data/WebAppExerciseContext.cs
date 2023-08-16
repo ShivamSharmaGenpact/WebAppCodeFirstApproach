0d2356bf97112db542d4f31bb1819cbd0e7ad7d7
Shivam082001 <shivamsh0808@gmail.com>
2023-08-16 15:45:30 +0530

    Code First Approach

---
+++ WebAppExercise/Data/WebAppExerciseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppExercise.Models;
+
+namespace WebAppExercise.Data
+{
+    public class WebAppExerciseContext : DbContext
+    {
+        public WebAppExerciseContext (DbContextOptions<WebAppExerciseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppExercise.Models.Course> Course { get; set; } = default!;
+    }
+}
---
+++ WebAppExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppExercise.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebAppExerciseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppExerciseContext") ?? throw new InvalidOperationException("Connection string 'WebAppExerciseContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
